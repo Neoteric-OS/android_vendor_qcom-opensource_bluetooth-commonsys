ed5a32dd932b323e2fde4fc0cfedcc7074a70da8
Jake Weinstein <jake@aospa.co>
2025-07-02 15:40:37 +0530

    bt: Only build test apps on eng builds.
    * These aren't needed for userdebug.
    
    Change-Id: I9340cac236d1ed5abbab09be84c6b88334e794a2
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -1,8 +1,8 @@
 #BT
 ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
 # BT Related Test app & Tools
-PRODUCT_PACKAGES_DEBUG += gatt_tool_qti_internal
-PRODUCT_PACKAGES_DEBUG += l2test_ertm
+PRODUCT_PACKAGES_ENG += gatt_tool_qti_internal
+PRODUCT_PACKAGES_ENG += l2test_ertm
 
 ifneq ($(TARGET_BOARD_TYPE),auto)
 # Set supported Bluetooth profiles to enabled
@@ -38,8 +38,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 ifneq ($(TARGET_HAS_LOW_RAM), true)
 PRODUCT_PACKAGES += Xpan
 PRODUCT_PACKAGES += privapp-permission-xpan.xml
-PRODUCT_PACKAGES += ChannelSoundingTestApp
-PRODUCT_PACKAGES += com.android.bluetooth.channelsoundingtestapp.xml
+PRODUCT_PACKAGES_ENG += ChannelSoundingTestApp
+PRODUCT_PACKAGES_ENG += com.android.bluetooth.channelsoundingtestapp.xml
 endif #TARGET_HAS_LOW_RAM
 
 endif #TARGET_BOARD_TYPE
